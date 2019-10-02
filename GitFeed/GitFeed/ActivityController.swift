175ee3ecb18a62eb0ffa49452f73d9e8d7109df1
Rui Cao <rayray199085@gmail.com>
2019-10-02 19:28:57 +1000

    Init the project

---
+++ GitFeed/GitFeed/ActivityController.swift
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2016-present Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ * distribute, sublicense, create a derivative work, and/or sell copies of the
+ * Software in any work that is designed, intended, or marketed for pedagogical or
+ * instructional purposes related to programming, coding, application development,
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works,
+ * or sale is expressly withheld.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+import RxSwift
+import RxCocoa
+import Kingfisher
+import RxSwiftExt
+
+class ActivityController: UITableViewController {
+  private let repo = "ReactiveX/RxSwift"
+
+  private let events = BehaviorRelay<[Event]>(value: [])
+  private let bag = DisposeBag()
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = repo
+
+    self.refreshControl = UIRefreshControl()
+    let refreshControl = self.refreshControl!
+
+    refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+    refreshControl.tintColor = UIColor.darkGray
+    refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+    refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+
+    refresh()
+  }
+
+  @objc func refresh() {
+    DispatchQueue.global(qos: .default).async { [weak self] in
+      guard let self = self else { return }
+      self.fetchEvents(repo: self.repo)
+    }
+  }
+
+  func fetchEvents(repo: String) {
+    let response = Observable
+      .from([repo])
+      .map( { URL(string: "https://api.github.com/repos/\($0)/events")!})
+      .map({ URLRequest(url: $0)})
+      .flatMap({ URLSession.shared.rx.response(request: $0)})
+      .share(replay: 1)
+    
+    response
+      .filter { (response, data) -> Bool in
+        return 200..<300 ~= response.statusCode}
+      .map({ try? JSONDecoder().decode([Event].self, from: $1) })
+      .unwrap()
+      .filter({ $0.count > 0})
+      .subscribe(onNext: { [weak self](events) in
+        self?.processEvents(events) })
+      .disposed(by: bag)
+  }
+  
+  func processEvents(_ newEvents: [Event]) {
+    
+  }
+
+  // MARK: - Table Data Source
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return events.value.count
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let event = events.value[indexPath.row]
+
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    cell.textLabel?.text = event.actor.name
+    cell.detailTextLabel?.text = event.repo.name + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+    cell.imageView?.kf.setImage(with: event.actor.avatar, placeholder: UIImage(named: "blank-avatar"))
+    return cell
+  }
+}
